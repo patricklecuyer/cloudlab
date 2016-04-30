0293becba08a1fa2733db72ddfd1e404f5a2dbff
Patrick Lecuyer <pl@conseilpatricklecuyer.com>
2016-04-30 18:15:33 -0400

    Added admin machine and SSL cert

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,5 +1,10 @@
 
-
+#Temporary certificate to initialize the ELB.  Let's Encrypt will replace once admin machine is up
+resource "aws_iam_server_certificate" "temp_cert" {
+  name = "temp_cert"
+  certificate_body = "${file("files/server.crt")}"
+  private_key = "${file("files/server.key")}"
+}
 
 resource "aws_elb" "web" {
   name = "cloudlab-elb-${var.hostname}"
@@ -11,8 +16,8 @@ resource "aws_elb" "web" {
   listener {
     instance_port     = 80
     instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
+    lb_port           = 443
+    lb_protocol       = "https"
   }
 
 }
@@ -46,6 +51,41 @@ resource "aws_instance" "jump" {
   }
 }
 
+resource "aws_instance" "admin" {
+    connection {
+      user = "ec2-user"
+      private_key = "~/.ssh/id_rsa"
+      bastion_host = "${aws_instance.jump.public_ip}"
+      private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  instance_type = "t2.small"
+
+  ami = "ami-08111162"
+
+  key_name = "${aws_key_pair.auth.id}"
+
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+
+
+  subnet_id = "${aws_subnet.admin.id}"
+
+  provisioner "file" {
+    source = "files/salt-master.conf"
+    destination = "/tmp/saltconf/master"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install epel-release",
+      "sudo yum -y install pip libgit2",
+      "pip install pygit2",
+      "curl -o bootstrap_salt.sh -L https://bootstrap.saltstack.com",
+      "sudo sh bootstrap_salt.sh -M -c /tmp/saltconf git stable"
+    ]
+  }
+}
 
 resource "aws_instance" "web" {
     connection {
